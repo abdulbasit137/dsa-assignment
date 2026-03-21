db93f18804010c1ceeb8ae7526bd4394b5fe531c
Abdul Basit <abdulbasit.13790@gmail.com>
2026-03-21 14:21:32 +0500

    Task-2

--- assignment-1.cpp
+++ assignment-1.cpp
@@ -7,38 +7,36 @@ struct Node {
 }*first = NULL;
 
 
-// funtion to insert end
+// insert at index
 void insert(Node *p, int index, int x) {
     Node *t;
     if (index < 0) return;
     t = new Node;
-    t -> data = x;
+    t->data = x;
+
     if (index == 0) {
-        t -> next = first;
+        t->next = first;
         first = t;
     } else {
-        for (int i = 0; i < index - 1 && p; i++)
-        {
-            p = p -> next;
+        for (int i = 0; i < index - 1 && p; i++) {
+            p = p->next;
         }
-        if (p)
-        {
-            t -> next = p -> next;
-            p -> next = t;
+        if (p) {
+            t->next = p->next;
+            p->next = t;
         }
-        
     }
 }
 
-// function for displaying list 
-void rdisplay(struct Node *p) {
+// display list
+void display(Node *p) {
     if (p != NULL) {
-        cout << p -> data << endl;
-        rdisplay(p  -> next);
+        cout << p->data << " ";
+        display(p->next);
     }
 }
 
-// Function for display in reverse 
+// reverse display (Task 1)
 void displayReverse(Node* head) {
     if (head == nullptr)
         return;
@@ -47,19 +45,39 @@ void displayReverse(Node* head) {
     cout << head->data << " ";  
 }
 
+// reverse linked list (Task 2)
+void reverseList() {
+    Node *prev = NULL;
+    Node *curr = first;
+    Node *next = NULL;
+
+    while (curr != NULL) {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+
+    first = prev;
+}
 
 int main() {
-    // Creating list
+
     insert(first, 0, 10);
     insert(first, 1, 20);
     insert(first, 2, 30);
     insert(first, 3, 40);
 
-    cout << "Normal Order: ";
-    rdisplay(first);
+    cout << "Original Playlist: ";
+    display(first);
 
-    cout << "\nReverse Order: ";
+    cout << "\nReverse Display (without changing list): ";
     displayReverse(first);
+
+    reverseList();
+
+    cout << "\nReversed Playlist (actual): ";
+    display(first);
 
     return 0;
 }
